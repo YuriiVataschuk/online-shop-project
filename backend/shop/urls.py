1e5cb29a6e38e60c5e31e9be6f6f826451f62334
yura.vataschuk <yura.vataschuk@gmail.com>
2023-10-20 20:49:40 +0200

    Filtration added

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -2,12 +2,14 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from shop.views import ProductViewSet, ProductNameViewSet
+from shop.views import ProductViewSet, ShirtsViewSet, SweatshirtsViewSet, HoodiesViewSet
 
 
 router = routers.DefaultRouter()
-router.register("products", ProductViewSet)
-router.register(r'products/name', ProductNameViewSet, basename='product-name')
+router.register("products", ProductViewSet, basename="products")
+router.register("shirts", ShirtsViewSet, basename="shirts")
+router.register("sweatshirts", SweatshirtsViewSet, basename="sweatshirts")
+router.register("hoodies", HoodiesViewSet, basename="hoodies")
 
 urlpatterns = [
     path("", include(router.urls))
