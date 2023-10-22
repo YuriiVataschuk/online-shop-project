86f5ff132cdc891a2ee4bd587386298ec175e6f8
Oleh <ochernyi04@gmail.com>
2023-10-22 15:00:29 +0300

    Remove unnecessary endpoints

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -2,14 +2,11 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from shop.views import ProductViewSet, ShirtsViewSet, SweatshirtsViewSet, HoodiesViewSet
+from .views import ProductViewSet
 
 
 router = routers.DefaultRouter()
 router.register("products", ProductViewSet, basename="products")
-router.register("shirts", ShirtsViewSet, basename="shirts")
-router.register("sweatshirts", SweatshirtsViewSet, basename="sweatshirts")
-router.register("hoodies", HoodiesViewSet, basename="hoodies")
 
 urlpatterns = [
     path("", include(router.urls))
