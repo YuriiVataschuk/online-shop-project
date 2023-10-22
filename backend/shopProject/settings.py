269edf02b6514b697bd0651f74d6d3178086e319
yura.vataschuk <yura.vataschuk@gmail.com>
2023-10-22 16:34:37 +0200

    Server DB

--- backend/shopProject/settings.py
+++ backend/shopProject/settings.py
@@ -81,12 +81,19 @@ WSGI_APPLICATION = "shopProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "snuffleupagus.db.elephantsql.com",
+        "NAME": "wckpjlwf",
+        "USER": "wckpjlwf",
+        "PASSWORD": "qSLeN3rgf5YJvTl9yq5uFrdRuWGn8exV",
     }
 }
+
 
 
 # Password validation
